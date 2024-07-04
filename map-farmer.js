81881f855f0b1e82b3d21b660be093db7c33fd65
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2024-07-04 11:01:42 +0200

    feat: scaffold

---
+++ map-farmer.js
@@ -0,0 +1,10 @@
+ScriptAPI.register('TW01', true, 'Hilmar Gústafsson', 'LiHRaM@users.noreply.github.com');
+window.TW01 = {};
+window.TW01.Library = (function () {})();
+
+window.TW01.Translation = (function () {})();
+window.TW01.Main = (function (Library, Translation) {});
+
+(() => {
+    window.TW01.Main.init();
+})();
